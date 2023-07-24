70c4492b0641ed85c0c75805c7303360afc950c9
Dave <140386550+david8609@users.noreply.github.com>
2023-07-24 21:12:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-main
+# import the time module
+import time
+
+# get the current time in seconds since the epoch
+seconds = time.time()
+
+print("Seconds since epoch =", seconds)	
+
+# Output: Seconds since epoch = 1672214933.6804628
+
+print("Printed immediately.")
+time.sleep(2.4)
+print("Printed after 2.4 seconds.")
